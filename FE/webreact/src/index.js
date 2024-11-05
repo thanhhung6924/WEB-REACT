9951c60c7c2c7aee2ee131c7d1d0a2642dfd334c
HUng <thanhhung6924@gmail.com>
2024-11-05 11:55:31 +0700

    Thông báo commit

--- FE/webreact/src/index.js
+++ FE/webreact/src/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
+import HomePage from "./page/user/homePage"
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <h1>ok main</h1>
+ <HomePage/>
 );
 
